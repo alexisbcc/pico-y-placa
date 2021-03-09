5801677324d3a45b7b1effea755dcd3f63c47931
Alexis Crespo <alexis-2cab@hotmail.com>
2021-03-09 18:36:57 -0500

    Added predictors to check days and time

---
+++ src/predictors.js
@@ -0,0 +1,45 @@
+// Methods
+const checkIsInTimeRange = (state) => ({
+  isInRegulatedTime: (requestedHour) => {
+    return state.timeRanges
+      .map(
+        (range) => requestedHour <= range.end && requestedHour >= range.start
+      )
+      .reduce((acc, current) => acc || current);
+  },
+});
+
+const checkIsInRegulatedDay = (state) => ({
+  isInRegulatedDay: (UTCDay, plateLastDigit) =>
+    state.platesNotCirculateByUTCDay[UTCDay].includes(plateLastDigit),
+});
+
+const TimeRangesToDecimalHours = (timeranges) => {
+  return timeranges.map((range) => {
+    return Object.keys(range).reduce((acc, item) => {
+      const [hours, minutes] = range[item].split(":");
+      return {
+        ...acc,
+        [item]: parseInt(hours, 10) + parseInt(minutes, 10) / 60,
+      };
+    }, {});
+  });
+};
+
+// predictor
+const circulationPredictor = (
+  timeRangesUnderControl,
+  platesNotCirculateByUTCDay
+) => {
+    let state = {
+      timeRanges: TimeRangesToDecimalHours(timeRangesUnderControl),
+      platesNotCirculateByUTCDay,
+    };
+    return {
+      ...checkIsInTimeRange(state),
+      ...checkIsInRegulatedDay(state),
+    };
+};
+
+module.exports.TimeRangesToDecimalHours = TimeRangesToDecimalHours;
+module.exports.circulationPredictor = circulationPredictor;
